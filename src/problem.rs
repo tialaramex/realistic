8bbd1e35cd02446b8f95b6e72f1042faac0aeb3a
Nick Lamb <git@tree.tlrmx.org>
2025-02-09 23:06:29 +0000

    Problem should implement Error and provide better documentation

--- src/problem.rs
+++ src/problem.rs
@@ -1,13 +1,37 @@
+// We need to refer to these types in the documentation
+#[allow(unused_imports)]
+use crate::{Rational, Real};
+
 /// Problems when either parsing or attempting Arithmetic with [`Real`] numbers
 /// also can occur when trying to make or convert to a [`Rational`]
 
 #[derive(Copy, Clone, Debug, PartialEq)]
+#[non_exhaustive]
 pub enum Problem {
+    /// Unspecified problem while parsing a number
     ParseError,
+    /// Tried to take the Square Root of a Negative, these values are Imaginary
     SqrtNegative,
+    /// Tried to divide by Zero, also arises if attempting to make a fraction with a zero
+    /// denominator
     DivideByZero,
+    /// The specified identifier in an expression was not found
     NotFound,
+    /// The expression has too few parameters to evaluate
     InsufficientParameters,
+    /// Tried to convert a floating point NaN, which has no equivalent
+    /// or evaluated the Logarithm of a non-positive value
     NotANumber,
+    /// Tried to convert a floating point Infinity which has no equivalent
     Infinity,
 }
+
+use std::fmt;
+
+impl fmt::Display for Problem {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+impl std::error::Error for Problem {}
